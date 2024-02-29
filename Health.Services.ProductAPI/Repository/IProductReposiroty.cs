74729fd4ee2c762c70c41f2b78eb1a8042414450
DESKTOP-A08F95P\Ray <jr071100@gmail.com>
2024-02-29 13:36:52 +0700

    setting up IProductReposiroty

--- Health.Services.ProductAPI/Repository/IProductReposiroty.cs
+++ Health.Services.ProductAPI/Repository/IProductReposiroty.cs
@@ -1,6 +1,12 @@
-﻿namespace Health.Services.ProductAPI.Repository
+﻿using Health.Services.ProductAPI.Models.Dtos;
+
+namespace Health.Services.ProductAPI.Repository
 {
 	public interface IProductReposiroty
 	{
+		Task<IEnumerable<ProductDto>> GetProducts();
+		Task<ProductDto> GetProductById(int productId);
+		Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+		Task<bool> DeleteProduct(int productId);
 	}
 }
