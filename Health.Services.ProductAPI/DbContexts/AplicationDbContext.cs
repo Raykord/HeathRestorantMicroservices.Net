7dd116e1c6e403594ea51544634a715fd5c051f2
DESKTOP-A08F95P\Ray <jr071100@gmail.com>
2024-02-29 13:11:19 +0700

    Add project files.

---
+++ Health.Services.ProductAPI/DbContexts/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Health.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Health.Services.ProductAPI.DbContexts
+{
+	public class AplicationDbContext : DbContext
+	{
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) 
+        {
+                  
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
